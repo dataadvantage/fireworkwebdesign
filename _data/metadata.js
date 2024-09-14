6c76e79f9c1047a2e2934cebec079217b35ec594
James Tansiri <tansirijames@gmail.com>
2024-09-14 09:31:30 +0700

    feat: update description

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 module.exports = {
 	title: "FIREWORK - Web design and development",
 	description:
-		"FIREWORK is a web design company in Bangkok, Thailand. We provide web design, web development, and digital marketing services.",
+		"We provide web design, web development, and digital marketing services.",
 	url: "https://www.fireworkwebdesign.com",
 	language: "en",
 	author: {
